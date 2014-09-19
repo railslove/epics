2c8112bbe044b3c6bc2ed1e3b7a8a772e62ac70e
Lars Brillert <lars@railslove.com>
2014-09-19 14:26:21 -0500

    remove pry

--- lib/epics/middleware/xmlsig.rb
+++ lib/epics/middleware/xmlsig.rb
@@ -7,7 +7,6 @@ class Epics::XMLSIG < Faraday::Middleware
 
   def call(env)
     @signer = Epics::Signer.new(@client, env["body"])
-    require "pry"; binding.pry
     @signer.digest!
     @signer.sign!
 
