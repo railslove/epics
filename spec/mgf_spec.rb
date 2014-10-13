b45e59775fa6867cafdaabd9b8a179038712a5aa
Marco Schaden <ms@donschado.de>
2014-10-13 12:28:59 +0200

    naive unit specs for MGF1
    I test most of the time directly down on the bytes because this is the easiest way

---
+++ spec/mgf_spec.rb
@@ -0,0 +1,42 @@
+RSpec.describe Epics::MGF1 do
+
+  describe '#generate' do
+    it { expect(subject.generate('foo', 0)).to eq('')}
+    it { expect(subject.generate('bar', 1)).to eq('8')}
+    it { expect(subject.generate('noIdea', 2).bytes).to eq([86, 174])}
+    it { expect(subject.generate('What', 4).bytes).to eq([238, 183, 195, 188])}
+    it { expect(subject.generate('ImDoing', 8).bytes).to eq([134, 205, 115, 236, 187, 67, 223, 2])}
+    it { expect { subject.generate('seed', 137438953473) }.to raise_error(ArgumentError, 'mask too long') }
+  end
+
+  describe '#i2osp' do
+    it { expect(subject.i2osp(1, 1).bytes).to eq([1])}
+    it { expect(subject.i2osp(1, 2).bytes).to eq([0, 1])}
+    it { expect(subject.i2osp(1, 3).bytes).to eq([0, 0, 1])}
+    it { expect(subject.i2osp(2, 1).bytes).to eq([2])}
+    it { expect(subject.i2osp(4, 4).bytes).to eq([0, 0, 0, 4])}
+    it { expect { subject.i2osp(256, 1) }.to raise_error(ArgumentError, 'integer too large') }
+  end
+
+  describe '#to_bytes' do
+    it { expect(subject.to_bytes(1).bytes).to eq([0, 0, 0, 0, 0, 0, 0, 1]) }
+    it { expect(subject.to_bytes(123456789).bytes).to eq([0, 0, 0, 0, 7, 91, 205, 21]) }
+    it { expect(subject.to_bytes(987654321).bytes).to eq([0, 0, 0, 0, 58, 222, 104, 177]) }
+  end
+
+  describe '#divceil' do
+    it { expect(subject.divceil(1, 1)).to eq(1) }
+    it { expect(subject.divceil(8, 2)).to eq(4) }
+    it { expect(subject.divceil(32, 4)).to eq(8) }
+    it { expect(subject.divceil(140, 21)).to eq(7) }
+    it { expect(subject.divceil(987654321, 123456789)).to eq(9) }
+  end
+
+  describe '#xor' do
+    it { expect(subject.xor('a', 'a').bytes).to eq([0]) }
+    it { expect(subject.xor('a', 'b').bytes).to eq([3]) }
+    it { expect(subject.xor('foo', 'bar').bytes).to eq([4, 14, 29]) }
+    it { expect(subject.xor('encyclopedia', 'aidepolcycne').bytes).to eq([4, 7, 7, 28, 19, 3, 3, 19, 28, 7, 7, 4]) }
+    it { expect { subject.xor('to raise', 'or not') }.to raise_error(ArgumentError, 'different length for a and b') }
+  end
+end
