49109b8a293dd8498484d0ad21e059ed99064cca
Lars Brillert <lars@railslove.com>
2014-10-14 14:56:40 +0200

    `content-type` is not used anymore

--- lib/epics/client.rb
+++ lib/epics/client.rb
@@ -147,7 +147,7 @@ class Epics::Client
   def connection
     @connection ||= Faraday.new do |faraday|
       faraday.use Epics::XMLSIG, { client: self }
-      faraday.use Epics::ParseEbics, {content_type: /.+/, client: self}
+      faraday.use Epics::ParseEbics, { client: self}
       # faraday.response :logger                  # log requests to STDOUT
       faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
     end
