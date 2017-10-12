4e5832a2c0e4ca15c59ac4538924e27350c3ee60
Lars Brillert <lars@railslove.com>
2017-10-12 17:40:22 +0200

    some ebics servers seems to have problems with the content type set, so we’re removing it. seems nonbreaking for all other servers we have in use.

--- lib/epics/middleware/xmlsig.rb
+++ lib/epics/middleware/xmlsig.rb
@@ -6,11 +6,12 @@ class Epics::XMLSIG < Faraday::Middleware
   end
 
   def call(env)
-    @signer = Epics::Signer.new(@client, env["body"])
+    @signer = Epics::Signer.new(@client, env['body'])
     @signer.digest!
     @signer.sign!
 
-    env["body"] = @signer.doc.to_xml(save_with: Nokogiri::XML::Node::SaveOptions::AS_XML)
+    env.request_headers['Content-Type']= ''
+    env['body'] = @signer.doc.to_xml(save_with: Nokogiri::XML::Node::SaveOptions::AS_XML)
 
     @app.call(env)
   end
