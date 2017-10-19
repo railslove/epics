519340eefa658a69f240d912020aa8e3d9b0b7f3
Lars Brillert <lars@railslove.com>
2017-10-19 19:22:30 +0200

    have specific fixtures per ruby engine, because the build xml might differ in term of namespace ordering, causing the hash value to be different but still valid

--- spec/orders/ini_spec.rb
+++ spec/orders/ini_spec.rb
@@ -1,5 +1,4 @@
 RSpec.describe Epics::INI do
-
   let(:client) { Epics::Client.new( File.open(File.join( File.dirname(__FILE__), '..', 'fixtures', 'SIZBN001.key')), 'secret' , 'https://194.180.18.30/ebicsweb/ebicsweb', 'SIZBN001', 'EBIX', 'EBICS') }
 
   before { allow(subject).to receive(:timestamp) { "2014-10-10T11:16:00Z" } }
@@ -10,8 +9,7 @@ RSpec.describe Epics::INI do
     specify { expect(subject.to_xml).to be_a_valid_ebics_doc }
 
     describe 'validate against fixture' do
-
-      let(:signature_order_data) { Nokogiri::XML(File.read(File.join( File.dirname(__FILE__), '..', 'fixtures', 'xml', 'ini.xml'))) }
+      let(:signature_order_data) { Nokogiri::XML(File.read(File.join( File.dirname(__FILE__), '..', 'fixtures', 'xml', RUBY_ENGINE, 'ini.xml'))) }
 
       it "will match exactly" do
         expect(Nokogiri::XML(subject.to_xml)).to be_equivalent_to(signature_order_data)
@@ -31,5 +29,4 @@ RSpec.describe Epics::INI do
       end
     end
   end
-
 end
