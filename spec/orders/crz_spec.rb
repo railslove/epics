21e833bdcb5a52f981378a4cf5618160844de911
Sahil Gadimbayli <sahil.gadimbay@gmail.com>
2022-09-15 01:23:39 +0400

    Adds CRZ order type and makes date range optional for CDZ
    - It would make sense to have CRZ order type where we already have CDZ
    - Date ranges are optional for given order types, thus allowing empty
      values.

--- spec/orders/crz_spec.rb
+++ spec/orders/crz_spec.rb
@@ -1,6 +1,6 @@
-RSpec.describe Epics::CDZ do
+RSpec.describe Epics::CRZ do
   let(:client) { Epics::Client.new( File.open(File.join( File.dirname(__FILE__), '..', 'fixtures', 'SIZBN001.key')), 'secret' , 'https://194.180.18.30/ebicsweb/ebicsweb', 'SIZBN001', 'EBIX', 'EBICS') }
-
+  
   context 'with date range' do
     subject { described_class.new(client, "2014-09-01", "2014-09-30") }
 
