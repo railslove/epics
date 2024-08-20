37c00046ddb13624c5f0b78ba91017fbb90238d9
Jonathan PHILIPPE <jphilippe@fluence.eu>
2024-08-20 10:06:34 +0200

    Simplified additional header options

--- spec/orders/c53_spec.rb
+++ spec/orders/c53_spec.rb
@@ -2,7 +2,7 @@ RSpec.describe Epics::C53 do
 
   let(:client) { Epics::Client.new( File.open(File.join( File.dirname(__FILE__), '..', 'fixtures', 'SIZBN001.key')), 'secret' , 'https://194.180.18.30/ebicsweb/ebicsweb', 'SIZBN001', 'EBIX', 'EBICS') }
 
-  subject { described_class.new(client, "2014-09-01", "2014-09-30") }
+  subject { described_class.new(client, from: "2014-09-01", to: "2014-09-30") }
 
   describe '#to_xml' do
     specify { expect(subject.to_xml).to be_a_valid_ebics_doc }
--- spec/orders/z54_spec.rb
+++ spec/orders/z54_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe Epics::Z54 do
   let(:client) { Epics::Client.new( File.open(File.join( File.dirname(__FILE__), '..', 'fixtures', 'SIZBN001.key')), 'secret' , 'https://194.180.18.30/ebicsweb/ebicsweb', 'SIZBN001', 'EBIX', 'EBICS') }
 
-  subject { described_class.new(client, "2014-09-01", "2014-09-30") }
+  subject { described_class.new(client, from: "2014-09-01", to: "2014-09-30") }
 
   describe '#to_xml' do
     specify { expect(subject.to_xml).to be_a_valid_ebics_doc }
