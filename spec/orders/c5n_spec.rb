c491ef0b8c0a486bbcc6fcda974ae33e867c4caf
Kostja93 <k.gontscharow@gmail.com>
2024-11-19 11:37:18 +0100

    Add specs

---
+++ spec/orders/c5n_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Epics::C5N do
+  let(:client) { Epics::Client.new( File.open(File.join( File.dirname(__FILE__), '..', 'fixtures', 'SIZBN001.key')), 'secret' , 'https://194.180.18.30/ebicsweb/ebicsweb', 'SIZBN001', 'EBIX', 'EBICS') }
+
+  subject { described_class.new(client, from: "2014-09-01", to: "2014-09-01") }
+
+  describe '#to_xml' do
+    specify { expect(subject.to_xml).to be_a_valid_ebics_doc }
+  end
+end
