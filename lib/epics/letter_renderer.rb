6297e1ca3947a0f0fe9622a30ed442281f884f60
Jonathan PHILIPPE <jphilippe@fluence.eu>
2025-03-20 14:24:31 +0100

    Fixes letter renderer

--- lib/epics/letter_renderer.rb
+++ lib/epics/letter_renderer.rb
@@ -14,7 +14,14 @@ class Epics::LetterRenderer
 
   alias_method :t, :translate
 
-  def_delegators :@client, :host_id, :user_id, :partner_id, :a, :x, :e
+  def_delegators :@client, :host_id, :user_id, :partner_id,
+                 :signature_version, :signature_key,
+                 :encryption_version, :encryption_key,
+                 :authentication_version, :authentication_key
+
+  alias_method :a, :signature_key
+  alias_method :e, :encryption_key
+  alias_method :x, :authentication_key
 
   def render(bankname)
     ERB.new(File.read(TEMPLATE_PATH)).result(binding)
